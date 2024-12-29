6707b57eea4942489db8ef454e8881abf5f7a71c
Ravindra <ravindra1s.siddavatam@gmail.com>
2024-12-29 03:37:04 -0600

    adding code

---
+++ src/programming/PlayingWithOptional.java
@@ -0,0 +1,23 @@
+package programming;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+
+public class PlayingWithOptional {
+
+	public static void main(String[] args) {
+
+		List<String> fruits = List.of("Apple","banana","cherry");
+		Predicate<? super String> predicate = fruit -> fruit.startsWith("b");
+		Optional<String> optional = fruits.stream().filter(predicate).findFirst();
+		System.out.println(optional);
+		//System.out.println(optional.isEmpty());
+		System.out.println(optional.isPresent());
+		
+		System.out.println(optional.get());
+
+	}
+
+}
